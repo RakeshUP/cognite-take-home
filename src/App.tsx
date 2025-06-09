011ecaf4cdcef69e254bae0a69e535db784dc7d4
Rakesh <parthiban.rakesh95@gmail.com>
2025-06-09 14:38:20 +0530

    First commit - Vite + Tailwind

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="flex justify-center items-center h-screen">
+        <p>Cognite Take Home</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
